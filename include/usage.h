2607d188fd4dde5b87613f4520d0033ebbeabf42
umar <jUmarB@protonmail.com>
2023-10-28 18:45:32 +0000

    first try  of libusage

---
+++ include/usage.h
@@ -0,0 +1,128 @@
+/**  @file usage.h 
+     @brief  build helper usage from getopt_long
+     
+     This file is not part of GNU C Library 
+     @Copyright (C) 2023 Umar Ba jUmarB@protonmail.com  OpenWire Studio .Lab
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+*/  
+
+#if !defined  (GETOPT_USAGE_H) 
+#define        GETOPT_USAGE_H 1 
+
+#ifdef __cplusplus
+extern "C"  {   
+#endif  
+
+#define   MXBUFF 0xff
+#define   RBN_MXBUFF ( MXBUFF >> 4 )
+
+#define  _nullable   ((void *) (0UL << 1) )   
+#define  _Nullable   _nullable 
+
+#ifdef  SHOWUPWUR  
+  #define  __must_check  __attribute__((warn_unused_result)) 
+#else 
+  #define  __must_check 
+#endif
+
+#ifdef GETOPT_SYNOPSIS
+  #define SYNOPSIS_INDEX 1
+  #define __condcheck(x,y)  x <= y 
+#else 
+  #define __condcheck(x,y)  x < y 
+#endif 
+
+
+#define  __ulog(__mesg ,  ... )  \
+  fprintf(stdout  , __mesg , ##__VA_ARGS__) ;  
+
+enum {
+   GETOPT_SIZE  
+#define  GETOPT_SIZE(__target_data) \
+  sizeof(__target_data)/sizeof(__target_data[0]) 
+} ;
+
+enum { 
+  BN2LONG = ~22 , 
+}; 
+
+typedef  struct  __getopt_usage_t  gopt_usage_t  ; 
+struct __getopt_usage_t { 
+  struct option *  opt ; 
+  int opt_size  ; 
+  char opt_desc[MXBUFF][MXBUFF] ;
+
+#ifdef GETOPT_SYNOPSIS 
+  union  { 
+     char synopsis[MXBUFF] ;
+     //... 
+  }; 
+#endif 
+
+} ;
+/** @fn  struct __getopt_usage_t  * init (struct option *   , int size )   
+ *  @brief initialize  getoptusage struct 
+ *  @param   struct option * 
+ *  @param   int size  :!NOTE: you'd better  user GETOPT_SIZE() macro  to get the size of  options
+ *  @return  struct  __getopt_usage_t  *
+ */ 
+struct __getopt_usage_t*  
+init( struct option * __opt , int size );
+
+/** @fn  struct __getopt_usage_t * init_(struct option * , int size , char * const * dl)
+ *  @brief like the init function above +  dump_desclist to feed  opt_desc 
+ *  @param   struct option * 
+ *  @param   int size  :!NOTE: you'd better  use GETOPT_SIZE() macro  to get the size of  options
+ *  @param   char * const *  dl  when (void *) 0 or NULL the init function above is used by default   
+ *  @return  struct  __getopt_usage_t  *
+ */ 
+struct __getopt_usage_t* 
+init_( struct option * __opt , int size  ,char * const * __description_list ) ; 
+
+/** good naming Macro shortcut*/
+#define  init_no_desc    init 
+#define  init_with_desc  init_ 
+
+
+/** @fn extern inline void endof_getoptusage ( struct __getopt_usage_t  *  __goptu ) 
+ *  @brief release allocated resources 
+ *  @param struct __getopt_usage_t  *  
+ */ 
+extern  inline void endof_getoptusage  ( struct __getopt_usage_t *  __restrict__   __goptu  ) {
+   if(__goptu  == (void *)0) return  ; 
+   free(__goptu) ; 
+} 
+
+
+/**  @fn void dump_desclist ( struct __getopt_usage_t * , char *const *  )  
+ *   @brief dump description list  in  getoptusage structure 
+ *   @param  __getopt_usage_t * 
+ *   @param  char * const *  
+ */
+void dump_desclist ( struct  __getopt_usage_t  * goptu  ,    char  * const *desclist ) ; 
+
+
+/** @fn  show_usage (struct __getopt_usage_t *)  
+ *  @brief print  description list dumped on gopt_usage_t 
+ */ 
+void  show_usage (struct __getopt_usage_t *  __goptu ,  char * const *  __argv ) ; 
+
+
+/** @brief basename program  manipulation to prettyfy */ 
+static char * __must_check root_basename (char  *const *  __argv ,char *__restrict__   __dumper)  ; 
+static char * __must_check fds_basename (char *basename) ; 
+
+#ifdef __cplusplus 
+} 
+#endif 
+
+#endif /** GETOPT_USAGE_H  */
---
+++ src/usage.c
@@ -0,0 +1,146 @@
+/**  @file getoptusage.h 
+     @brief  build helper usage from getopt_long using options structure 
+
+     @Copyright (C) 2023 Umar Ba jUmarB@protonmail.com  , OpenWire Studio .Lab
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+*/
+
+
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <stdio.h> 
+#include <err.h> 
+#include <libgen.h>  
+#include <getopt.h>
+
+#include "usage.h"
+
+
+struct  __getopt_usage_t *  init ( struct  option *  opt   , int size  )  
+{
+
+  if ( opt ==  _nullable)  return  _nullable ;  
+   
+  struct __getopt_usage_t *  goptu = (struct __getopt_usage_t*) malloc(sizeof(*goptu)) ;  
+  
+  goptu->opt = opt ; 
+  goptu->opt_size  =   size  ;
+#ifdef GETOPT_SYNOPSIS 
+   explicit_bzero(goptu->synopsis,MXBUFF);  
+#endif 
+  return goptu;  
+}
+
+
+struct __getopt_usage_t * init_(struct option *  opt , int size ,  char * const *  desclist)  
+{
+   if (desclist == _nullable)  
+   { 
+     return init(opt  , size) ; 
+   } 
+
+   struct __getopt_usage_t *  goptu = (struct __getopt_usage_t *) malloc(sizeof(*goptu)) ; 
+   goptu->opt = opt ; 
+   goptu->opt_size  =   size  ; 
+
+#ifdef GETOPT_SYNOPSIS 
+   explicit_bzero(goptu->synopsis,MXBUFF);  
+#endif 
+   dump_desclist(goptu ,  desclist) ;   
+
+   return goptu; 
+
+    
+}
+
+void dump_desclist ( struct  __getopt_usage_t  * goptu  ,    char  * const *desclist ) 
+{
+  int  desclist_index = 0 ; 
+  while ( desclist_index  <=goptu->opt_size ) 
+  {
+
+    memcpy (
+        (goptu->opt_desc+desclist_index) , 
+        desclist[desclist_index] ,
+        strlen(desclist[desclist_index]))  ; 
+    desclist_index++ ;
+  }
+}
+
+void
+show_usage( struct __getopt_usage_t * goptu , char * const *  argv   )  
+{
+   if (goptu == _nullable)   
+   {
+     errx(~0 ,"nil <nothing to show>") ; 
+   }
+ 
+   char rbasename[RBN_MXBUFF] = {0}  ;
+   if ( argv !=  _nullable)  
+   {
+     (void*) root_basename(argv ,  rbasename); 
+#ifdef  basename 
+     memcpy(rbasename , __xpg_basename(rbasename)  , strlen(rbasename)) ; 
+#endif 
+
+     fprintf(stdout , "%s Usage:\n" , rbasename ) ; 
+   }
+   
+   int index  = 0 ;
+   int index_options = 0 ; 
+#ifdef   GETOPT_SYNOPSIS  
+   index = 1;  
+   memcpy(goptu->synopsis ,  goptu->opt_desc[index^SYNOPSIS_INDEX] , MXBUFF) ; 
+   fprintf(stdout ,  "\t%s\n" ,  goptu->synopsis); 
+   index_options =1  ; 
+#endif 
+
+   puts("Options:") ;
+   while(__condcheck(index , goptu->opt_size)) 
+   {
+      fprintf(stdout  , "\t-%-1c, --%-2s\t" ,  goptu->opt[index - index_options].val ,  goptu->opt[index-index_options].name); 
+      fprintf(stdout, "%-10s\n", goptu->opt_desc[index]); 
+      index++ ;  
+   }
+}
+
+
+static char * __must_check  root_basename (char * const * argv ,  char  * restrict rb_dump ) 
+{
+  char *localbasname =(char *) (*argv+0) ; 
+   
+  if (strlen(localbasname) >  RBN_MXBUFF)    
+  {
+     errx(BN2LONG,  "root basename too long !");  
+  }
+
+  fds_basename(localbasname) ;  
+  memcpy(rb_dump , (localbasname) ,  RBN_MXBUFF) ; 
+  return rb_dump ; 
+  
+}
+
+
+static char * __must_check fds_basename  (  char *basename ) 
+{
+  
+  char dot_start = 0x2e ;
+   /** looking  for  './'*/
+  if ( (*basename+0) ==dot_start  &&  (*basename+1) ==  dot_start++) 
+  {
+    return memcpy(basename ,   (basename+2) , strlen(basename)) ; 
+  } 
+  
+  return  basename ; 
+}
+ 
